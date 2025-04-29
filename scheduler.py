8f537c65ce373228c7198749295ee1349e1219df
MahenJuaa <trickster1140@gmail.com>
2025-04-29 17:31:34 +0700

    Add files via upload

--- scheduler.py
+++ scheduler.py
@@ -7,7 +7,7 @@ def setup_schedule():
     schedule.every().monday.at("12:00").do(auto_presensi, keterangan="Patt.I-3B")
     schedule.every().monday.at("14:30").do(auto_presensi, keterangan="Patt.I-3A")
     schedule.every().tuesday.at("07:00").do(auto_presensi, keterangan="Patt.III-3A")
-    schedule.every().tuesday.at("14:30").do(auto_presensi, keterangan="Patt.I-3D")
+    schedule.every().tuesday.at("15:00").do(auto_presensi, keterangan="Patt.I-3D")
     schedule.every().wednesday.at("09:30").do(auto_presensi, keterangan="Patt.III-3A")
     schedule.every().wednesday.at("13:00").do(auto_presensi, keterangan="Lab Rancang Bangun Perangkat Lunak")
     schedule.every().wednesday.at("15:00").do(auto_presensi, keterangan="Lab Jaringan")
